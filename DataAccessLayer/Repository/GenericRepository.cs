c60ade11ad1eadb14458d4d63496ff185cfb8994
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-07-04 22:21:45 +0300

    Update

--- DataAccessLayer/Repository/GenericRepository.cs
+++ DataAccessLayer/Repository/GenericRepository.cs
@@ -47,7 +47,7 @@ namespace DataAccessLayer.Repository
         {
             using var c = new Context();
             c.Update(t);
-            c.SaveChanges(); // Değişiklileri Kaydet
+            c.SaveChanges(); // Değişiklileri Kaydetme
         }
     }
 }
